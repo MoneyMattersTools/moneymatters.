585eda24ff050d402ba50f374222510e5b691005
emoss <emosser21@gmail.com>
2026-07-31 14:30:45 -0400

    chore: remove temp diag function after confirming tool_feedback migration not yet applied

--- netlify/functions/_diag-check-feedback-table.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async () => {
-  const url = process.env.SUPABASE_URL.replace(/\/$/, '').replace(/\/rest\/v1$/, '');
-  const key = process.env.SUPABASE_SERVICE_ROLE_KEY;
-  const headers = { apikey: key, Authorization: `Bearer ${key}` };
-  const res = await fetch(`${url}/rest/v1/tool_feedback?select=id&limit=1`, { headers });
-  const text = await res.text();
-  return { statusCode: 200, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ status: res.status, body: text }) };
-};
