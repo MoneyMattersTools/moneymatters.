016ab3bc301695cef7b2857da42273f2d374d16a
emoss <emosser21@gmail.com>
2026-07-31 11:18:38 -0400

    temp: diag function to verify §37 item 3 advisor-intake submission landed in Supabase

---
+++ netlify/functions/_diag-check-advisor-submission.js
@@ -0,0 +1,8 @@
+exports.handler = async () => {
+  const url = process.env.SUPABASE_URL.replace(/\/$/, '').replace(/\/rest\/v1$/, '');
+  const key = process.env.SUPABASE_SERVICE_ROLE_KEY;
+  const headers = { apikey: key, Authorization: `Bearer ${key}` };
+  const res = await fetch(`${url}/rest/v1/advisor_review_requests?email=eq.mm-qa-persistent@web-library.net&order=created_at.desc&limit=1`, { headers });
+  const data = await res.json();
+  return { statusCode: 200, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) };
+};
