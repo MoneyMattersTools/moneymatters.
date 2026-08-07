d5285f1be0784386aad12daed28d14e9df159ab2
emoss <emosser21@gmail.com>
2026-08-07 06:17:24 -0400

    chore: remove round-35 E2E test diagnostic functions

--- netlify/functions/_diag-recheck-row.mjs
+++
@@ -1,8 +0,0 @@
-import supabaseLib from './lib/supabase.js';
-
-const { findOneByFilters, encodeEq } = supabaseLib;
-
-export default async () => {
-  const row = await findOneByFilters('advisors', [`id=${encodeEq('c147cc97-ad03-42f4-9f26-985eeccd0bb8')}`]);
-  return new Response(JSON.stringify(row, null, 2), { headers: { 'Content-Type': 'application/json' } });
-};
